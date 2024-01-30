6e8e1ea13956b61161a3c68bfa2927e0b5991de5
Fabiano Liberatori <fabiano29585@gmail.com>
2024-01-30 12:08:04 -0300

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.get('/', (_request, response) => {
 app.use(express.json());
 
 // ...
+// For Initial Commit
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
